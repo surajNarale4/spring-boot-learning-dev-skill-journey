e6edb6b4b72a4befa4d7207acc403f4e72ecf971
surajNarale4 <rajnarale415@gmail.com>
2026-05-02 10:46:55 +0530

    user entity added for representing current authorised user

---
+++ src/main/java/com/prod/entities/User.java
@@ -0,0 +1,39 @@
+package com.prod.entities;
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    Long id;
+    @Column(unique = true)
+    private String email;
+    private String password;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public @Nullable String getPassword() {
+        return "";
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+}
